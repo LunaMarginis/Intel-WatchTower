0d1917c254136c500978e44c188c136703e3f156
Aadi Chandra <adithya.chandra.in@gmail.com>
2025-05-25 23:34:09 +0530

    Update hash.py

--- hash.py
+++ hash.py
@@ -73,7 +73,7 @@ markdown_content = "\n".join(md_lines)
 # Write to README.md (overwrites or create)
 with open("README.md", "w", encoding="utf-8") as f:
     f.write("# IOC Summary\n\n")
-    f.write("\nAutomated Hash Collections: Every day at 02:00 UTC\n")
+    f.write("Automated Hash Collections: Every day at 02:00 UTC.\n\n")
     f.write("This table shows top 10 malware names with their unique hashes and counts.\n\n")
     f.write(markdown_content)
 
